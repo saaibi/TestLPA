6249828c33653f01fcdba55f35b50e51f6b6901b
saaibi yesid florez <saaibiyesid@hotmail.com>
2018-09-21 12:12:35 -0500

    Config Test
    #4

--- server/models/clients.js
+++ server/models/clients.js
@@ -1,16 +1,19 @@
-const  mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // Create Schema
 const ClientSchema = new Schema({
-	firstName: { type: String, required: true},
-    lastName: {type: String, required: true},
-    document: {type: String, requerid: true},
-	credit: {
-        value : {type: Number, default: 0},
-        interest : {type: Number, default: 0 },
-        payDaily : {type: Number, default: 0 }
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    document: { type: String, requerid: true },
+    credit: {
+        value: { type: Number, default: 0 },
+        interest: { type: Number, default: 0 },
+        payDaily: { type: Number, default: 0 }
     }
-})
+},
+    {
+        versionKey: false
+    })
 
 module.exports = mongoose.model('Client', ClientSchema);
