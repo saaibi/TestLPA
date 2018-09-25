3e7810052b81a59fb97c00321c60f1a3c5599865
saaibi yesid florez <saaibiyesid@hotmail.com>
2018-09-24 23:21:59 -0500

    Modals Edit
    #7

--- app/src/js/components/Clients/Grid/Partials/ActionBarGrid.jsx
+++ app/src/js/components/Clients/Grid/Partials/ActionBarGrid.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Modal } from 'react-materialize';
+
 import Edit from '../../../Common/Button';
 import Delete from '../../../Common/Button';
 import View from '../../../Common/Button';
 
-const ActionBarGrid = ({ id, onEdit, onDelete }) => (
+
+const ActionBarGrid = ({ id, onEdit, onDelete, onView }) => (
 	<div>
-		<Edit
-			className="btn-floating green"
-			texto="+"
-			onClick={ onEdit }
-		/>
+		<Modal
+			header='Edit'
+			trigger={<Edit
+				className="btn-floating green pulse"
+				texto="+"
+				onClick={onEdit}
+			/>}>
+			<p>L est laborum</p>
+		</Modal>
+
 		<Delete
 			className="btn-floating red"
 			texto="-"
-			onClick={ onDelete }
+			onClick={onDelete}
 		/>
 		<View
-			className="btn-floating cyan pulse"
+			className="btn-floating cyan "
 			texto="º"
-			onClick={ onDelete }
+			onClick={onView}
 		/>
 	</div>
 );
